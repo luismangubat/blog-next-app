8826e92a77a5e3af597194b534593270957b2eb8
luismangubat <joseluismangubat@gmail.com>
2021-11-11 00:18:25 -0500

    Initialized Next App

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link';
+import Loader from '../components/Loader';
+
+export default function Home() {
+  return (
+    <div >
+      <Loader show/>
+      Hello
+    </div>
+  )
+}
